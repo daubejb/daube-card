50bcc19a5332b916bbe4ba00e6b3e4058289d285
Jeffrey B. Daube <daubejb@gmail.com>
2017-09-05 22:18:43 -0400

    implement basic card and style

--- daube-card.js
+++ daube-card.js
@@ -9,13 +9,28 @@ template.innerHTML = `
       color: rgba(0,0,0,0.87);
       margin: 0;
       padding: 0;
+    }
 
     :host([hidden]) {
       display: none;
     }
 
+    .daubecard {
+      max-width: 45rem;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+      transition: 0.3s;
+      border-radius: 3px;
+      padding: 1.25rem 2rem 4rem 2rem;
+      margin: 0 auto;
+      background-color: white;
+    }
+
   </style>
-`;
+
+  <div class="daubecard">
+    <slot></slot>
+  </div>
+`
 
 if (window.ShadyCSS) {
   ShadyCSS.prepareTemplate(template, 'daube-card');
@@ -25,7 +40,7 @@ class DaubeCard extends HTMLElement {
   static get observedAttributes() {}
   constructor() {
     super();
-    
+
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
